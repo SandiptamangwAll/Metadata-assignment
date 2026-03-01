921054ea4a7819521431c5c20c7d81269ba982f4
WeAreDo <sandiptamangw1n3@gmail.com>
2026-03-01 13:34:33 +0545

    Delete unitest.py

--- unitest.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from metadata import MetadataTag
-
-class TestMetadata(unittest.TestCase):
-    def test_sensitive_tag(self):
-        tag = MetadataTag("gps_latitude", "27.7", "GPSInfo")
-        self.assertTrue(tag.is_sensitive())
